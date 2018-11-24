90ed53a85db39584ccc4995ce325e9d539c97da1
agnik <agnik@mit.edu>
2018-11-24 14:39:02 -0500

    adding to predict

---
+++ demo5.m
@@ -0,0 +1,58 @@
+
+
+clear all;
+rng default;
+
+h.alpha = 1.5;
+h.var_theta = 2;
+h.var_mu = 1;
+h.var_r = 1;
+
+D(1) = init_D_from_txt('hourglass_rewards.txt');
+% for i = 1:length(D)
+%     tic 
+%     [samples, post] = sample(D(i), h, 1000);
+%     for j = 1:length(samples)
+%         H(i,j) = samples(j);
+%         P(i,j) = post(j);
+%     end
+%     toc
+% end
+
+save demo4.mat
+
+% load demo1.mat;
+
+figure;
+
+k = 5;
+for i = 1:length(D)
+    post = P(i,:);
+    [~,I] = maxk(post, k);
+    for j = 1:k
+        subplot(length(D),k, (i-1)*k+j);
+        plot_H(H(i,I(j)), D(i));
+        if j == ceil(k/2);
+            %ylabel(D(i).name);
+            title(D(i).name);
+        end
+        set(gca, 'xtick', []);
+        set(gca, 'ytick', []);
+    end
+end
+
+[p, mu] = predict(D, h, 100, 1000, 10, 1, ids) 
+
+%figure;
+%        subplot(length(D),5, (i-1)*5+j);
+%        plot_H(H(i,j), D(i));
+%        if j == 1
+%            ylabel(D(i).name);
+%        end
+
+%D(2) = init_D_from_csv('/Users/momchil/Dropbox/Research/chunking/occluder3D_data/100931.csv');
+
+
+%H = MAP_H(D, h);
+%plot_H(H, D);
+%title(D.name);
--- predict.m
+++ predict.m
@@ -1,39 +1,40 @@
-function [p_1, p_2] = predict(D, h, nsamples, burnin, lag, m, tau, ids)
+function [p, mu] = predict(D, h, M, burnin, lag, tau, ids)
     %
     % D, h, nsmaples, burnin, and lag are the parameters for the sample fn
-    % m: # of times to sample H
+    % M: # of times to sample H
     % tau: temperature for Softmax function
     % ids: vector of length 2 where ids[1] is the node ID for the first
     %      neigbhor and ids[2] is the node ID for the second neighbor.
     %
 
     % Take m samples of H
-    for i = 1:m
-        sample(D, h, nsamples, burnin, lag); % TODO fill in params
-    end
+    % w is probabilities (normalized weights)
+    [H, w] = sample(D, h, M, burnin, lag);
+    w = w/sum(w);
+    %for i = 1:m
+     %   sample(D, h, nsamples, burnin, lag); % TODO fill in params
+    %end
     
     % Expected value of mu_1 given observed reward 
     % (equal to sum over m of theta_c1 in each sample of H)
-    mu_1_exp = 0;
-    for i = 1:m
-        curSampleH = sample(D, h, nsamples, burnin, lag); 
-        mu_1_exp = mu_1_exp + curSampleH.theta_c1;
-    end
+    %mu_1 = 0;
+    for i = 1:D.G.N
+        mu(i) = 0;
+        for m = 1:M
+            mu(i) = mu(i) + H(m).theta(H(m).c(i));
+        end
+        mu(i) = mu(i)/M;
+    end 
     
     % Expected value of mu_2 given observed reward 
     % (equal to sum over m of theta_c2 in each sample of H)
-    mu_2_exp = 0;
-    for i = 1:m
-        curSampleH = sample(D, h, nsamples, burnin, lag); 
-        mu_2_exp = mu_2_exp + curSampleH.theta_c2;
-    end
     
     % Pass through softmax function
     % P(choose 1 | E[mu_1], E[mu_2]) = exp(E[mu_1]/tau)/(exp(E[mu_1]/tau) +
     %   exp(E[mu_3]/tau)); draw from this multinomial distribution (??)
-    prob = exp(mu_1_exp/tau)/((mu_2_exp/tau) + exp(mu_3_exp/tau));
+    p = exp(mu(ids)/tau)/(sum(exp(mu(ids)/tau)));
     
     % Return p_1 and p_2
-    p_1 = None;
-    p_2 = None;
+    %p_1 = None;
+    %p_2 = None;
 end
