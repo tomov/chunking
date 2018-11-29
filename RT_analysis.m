4b7f95f514f333332ce0cc7c6b62dc12546d9a0e
Momchil Tomov <tomov90@gmail.com>
2018-11-29 14:34:31 -0500

    RT_anal

--- RT_analysis.m
+++ RT_analysis.m
@@ -3,12 +3,15 @@ clear;
 
 sem = @(x) std(x) / sqrt(length(x));
 
+% --------- MAP view -------------
+
 % for subway 10
 %
 action_chunk_transitions{1} = [1 2; 2 3; 4 5; 5 6; 10 9; 9 8; 8 7];
 state_chunk_transitions{1} = flip(action_chunk_transitions{1},2);
 bridges{1} = [3 4; 4 3; 1 10; 10 1; 7 6; 6 7];
 dirname{1} = 'exp/results/subway10_map/';
+nrows{1} = 81;
 
 % for subway 9 (bad chunks)
 %
@@ -16,6 +19,7 @@ action_chunk_transitions{2} = [1 2; 2 3; 4 5; 5 6; 9 8; 8 7];
 state_chunk_transitions{2} = flip(action_chunk_transitions{2},2);
 bridges{2} = [3 4; 4 3; 1 9; 9 1; 7 6; 6 7];
 dirname{2} = 'exp/results/subway9_map/';
+nrows{2} = 81;
 
 % for subway 9 (good chunks)
 %
@@ -23,6 +27,35 @@ action_chunk_transitions{3} = [1 9; 8 7; 7 6; 2 3; 3 4; 4 5];
 state_chunk_transitions{3} = flip(action_chunk_transitions{3},2);
 bridges{3} = [8 9; 9 8; 6 5; 5 6; 1 2; 2 1];
 dirname{3} = 'exp/results/subway9_map_goodchunks/';
+nrows{3} = 81;
+
+% --------------- adjacent view ---------------
+
+% for subway 10
+%
+action_chunk_transitions{4} = [1 2; 2 3; 4 5; 5 6; 10 9; 9 8; 8 7];
+state_chunk_transitions{4} = flip(action_chunk_transitions{4},2);
+bridges{4} = [3 4; 4 3; 1 10; 10 1; 7 6; 6 7];
+dirname{4} = 'exp/results/subway10_repro';
+nrows{4} = 83;
+
+% for subway 9 (bad chunks)
+%
+action_chunk_transitions{5} = [1 2; 2 3; 4 5; 5 6; 9 8; 8 7];
+state_chunk_transitions{5} = flip(action_chunk_transitions{5},2);
+bridges{5} = [3 4; 4 3; 1 9; 9 1; 7 6; 6 7];
+dirname{5} = 'exp/results/subway9/';
+nrows{5} = 81;
+
+% for subway 9 (good chunks)
+%
+action_chunk_transitions{6} = [1 9; 8 7; 7 6; 2 3; 3 4; 4 5];
+state_chunk_transitions{6} = flip(action_chunk_transitions{6},2);
+bridges{6} = [8 9; 9 8; 6 5; 5 6; 1 2; 2 1];
+dirname{6} = 'exp/results/subway9_goodchunks/';
+nrows{6} = 81;
+
+
 
 action_chunk_RTs = [];
 state_chunk_RTs = [];
@@ -33,7 +66,7 @@ bridge_RTs = [];
 for f = 1:length(dirname)
     fprintf('\n\n ---------------- Data dir %s -------------- \n\n', dirname{f});
 
-    [data, Ts] = load_data(dirname{f}, 81);
+    [data, Ts] = load_data(dirname{f}, nrows{f});
 
     % RT analysis
     %
