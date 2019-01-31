8d0d45e11362c86894a57ab4e036ef1577953147
Momchil Tomov <tomov90@gmail.com>
2019-01-31 14:50:28 -0500

    debug print

--- printmex.cpp
+++ printmex.cpp
@@ -3,11 +3,14 @@
 #include "stdarg.h"
 
 // helper thingy to printf to matlab; cannot do it directly in cpp file b/c including mex.h (the C version) and mex.hpp (the C++ version) is incompatible. We need former for mexPrintf, latter for C++ stuff
+// see https://www.mathworks.com/help/matlab/apiref/mexprintf.html
 //
 
 void printThis( const char* format, ... ) {
     va_list args;
     va_start( args, format );
-    mexPrintf( format, args );
+    char msg[256];
+    vsprintf(msg, format, args);
+    mexPrintf( msg ); // wish there was a vmexPrintf...
     va_end( args );
 }
