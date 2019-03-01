fdc0e58ccd5ed06f9b2cc01f3924ca356c24e7c1
Momchil Tomov <tomov90@gmail.com>
2019-03-01 13:45:21 -0500

    test_loglik_c_stress

---
+++ test_loglik_c_stress.m
@@ -0,0 +1,17 @@
+% stresstest loglik_c vs. loglik
+
+files = {'schapiro_N=1_alpha=1_nsamples=100000.mat', ...
+         'solway1_N=1_alpha=1.0000_nsamples=100000', ...
+         'solway2_N=1_alpha=1.0000_nsamples=100000.mat'};
+
+for f = 1:length(files)
+    file = files{f};
+    load(file);
+
+    for i = 1:length(H_all{1})
+        l1 = loglik(H_all{1}(i), D, h);
+        l2 = loglik_c(H_all{1}(i), D, h);
+
+        assert(abs(l1 - l2) < 1e-9);
+    end
+end
